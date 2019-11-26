d1700977c1a06d5ef12a198c5bcda0f90231f542
Sirak Zeray <sirakzeray@gmail.com>
2019-11-25 21:24:19 -0500

    Firt commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const keys = require("./config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 5000;
+
+//Index Route
+app.get("/", (req, res) => {
+  res.render("index", {
+    stripePublishableKey: keys.stripePublishableKey
+  });
+});
+
+app.post("/charge", (req, res) => {
+  const amount = 2500;
+
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken
+    })
+    .then(customer => {
+      stripe.charges
+        .create({
+          amount,
+          description: "Web development Ebook",
+          currency: "usd",
+          customer: customer.id
+        })
+        .then(charge => res.json(charge))
+        .catch(err => res.json({ err: err }));
+    });
+});
+
+app.listen(port, () => {
+  console.log(`server started on ${port}`);
+});
